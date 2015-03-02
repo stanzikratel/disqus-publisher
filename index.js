c86579cc8e7249f0da31e707b6599b1d81c7ce83
Scott Beaudreau <sjbeaudreau@gmail.com>
2015-03-01 23:28:59 -0600

    Adding a shell index.js to start working with elasticsearch

---
+++ index.js
@@ -0,0 +1,16 @@
+var _ = require('underscore');
+var pretty = require('pretty-data').pd;
+var es = require('elasticsearch');
+var disqusExportParser = require('disqus-export-parser');
+
+
+exports.cli = function () {
+	// get the parameters
+	var filename = process.argv[2];
+
+	// Call publish
+};
+
+exports.publish = function (inputFileName, searchServerHost, searchServerPort, searchIndex) {
+}
+
